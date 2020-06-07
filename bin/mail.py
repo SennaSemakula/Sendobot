429c77ed941704cd484dbe5c0eb80190c175d9c9
Senna Semakula-Buuza <ssemakul@cisco.com>
2020-06-07 20:21:18 +0100

    only send todos to myself

--- bin/mail.py
+++ bin/mail.py
@@ -30,14 +30,12 @@ def read_file(category):
     with open(f"{category}_news.txt", "r") as f_obj:
         return f_obj.read().strip().encode('ascii', 'ignore').decode('ascii')
 
-def create_body(category):
+def create_body(category, list_todos=""):
     contents = ""
     contents += " \"We have two lives and the second one begins when we realise we only have one - Conficious\"\n\n"
-    todos = TODO_LIST_TECH if category == "tech" else TODO_LIST_MERGER
-    todos = create_todos(todos)
-    contents += f'{todos}\n'
+    contents += f'{list_todos}\n'
     contents += read_file(category)
-    subject=f"Sendobot TODOs & {category.title()} Newsletter"
+    subject=f"Sendobot {category.title()} Newsletter"
     email_text = f"""\
 From: {USER_EMAIL}
 To: {RECIPIENTS}
@@ -46,23 +44,27 @@ Subject: {subject}
 {contents}
 
 From Sendoka bot :)
-"""
+""".format(todos=list_todos)
     return email_text
 
 def main():
-    tech_body = create_body("tech")
-    merger_body = create_body("merger")
+    todos = create_todos(TODO_LIST_TECH)
 
     try:
         with SMTP_SSL('smtp.gmail.com', 465) as smtp:
             smtp.ehlo()
             smtp.login(USER_EMAIL, TOKEN)
             for i, recipient in enumerate(RECIPIENTS):
-                smtp.sendmail(USER_EMAIL, recipient, tech_body)
-                print(f"Tech news mail sent to: {recipient}")
                 if i == 0:
+                    tech_body = create_body("tech", todos)
+                    merger_body = create_body("merger", "")
                     smtp.sendmail(USER_EMAIL, recipient, merger_body)
                     print(f"Merger & Acquisitions news mail sent to: {recipient}")
+                else:
+                    tech_body = create_body("tech", "")
+
+                smtp.sendmail(USER_EMAIL, recipient, tech_body)
+                print(f"Tech news mail sent to: {recipient}")
     except Exception as e:
         print(e)
 
